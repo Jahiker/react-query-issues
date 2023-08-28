3a26aa80f1721752efd262ef00e176fe02c2d24a
jahiker <rojasjahiker@gmail.com>
2023-08-28 00:46:29 -0400

    [add] issues labels

---
+++ src/hooks/useLabels.tsx
@@ -0,0 +1,66 @@
+import { useQuery } from "@tanstack/react-query";
+import { githubApi } from "../api/githubApi";
+import { Label } from "../issues/interfaces/label";
+import { sleep } from "../helpers/sleep";
+
+export const useLabels = () => {
+  const getLabels = async (): Promise<Label[]> => {
+    await sleep(2);
+
+    const { data } = await githubApi.get<Label[]>("/labels");
+    console.log("🚀 ~ file: LabelPicker.tsx:6 ~ getLabels ~ data:", data);
+
+    return data;
+  };
+
+   /**
+    * initialData => sustituye a la data del fetching mientras se cumple el staleTime 
+    * placeholderData => solo se muentra mientras se realiza el fetching
+    */
+  const labelsQuery = useQuery(["labels"], getLabels, {
+    refetchOnWindowFocus: false,
+    staleTime: 1000 * 60 * 60,
+    // initialData: [
+    //   {
+    //     id: 725156255,
+    //     node_id: "MDU6TGFiZWw3MjUxNTYyNTU=",
+    //     url: "https://api.github.com/repos/facebook/react/labels/good%20first%20issue%20(taken)",
+    //     name: "good first issue (taken)",
+    //     color: "b60205",
+    //     default: false,
+    //     description: "",
+    //   },
+    //   {
+    //     id: 717031390,
+    //     node_id: "MDU6TGFiZWw3MTcwMzEzOTA=",
+    //     url: "https://api.github.com/repos/facebook/react/labels/good%20first%20issue",
+    //     name: "good first issue",
+    //     color: "6ce26a",
+    //     default: true,
+    //     description: "",
+    //   },
+    // ],
+    placeholderData: [
+      {
+        id: 725156255,
+        node_id: "MDU6TGFiZWw3MjUxNTYyNTU=",
+        url: "https://api.github.com/repos/facebook/react/labels/good%20first%20issue%20(taken)",
+        name: "good first issue (taken)",
+        color: "b60205",
+        default: false,
+        description: "",
+      },
+      {
+        id: 717031390,
+        node_id: "MDU6TGFiZWw3MTcwMzEzOTA=",
+        url: "https://api.github.com/repos/facebook/react/labels/good%20first%20issue",
+        name: "good first issue",
+        color: "6ce26a",
+        default: true,
+        description: "",
+      },
+    ],
+  });
+
+  return { labelsQuery };
+};
--- src/issues/components/LabelPicker.tsx
+++ src/issues/components/LabelPicker.tsx
@@ -1,14 +1,38 @@
+import { FC } from "react";
+import { useLabels } from "../../hooks/useLabels";
+import { LoadingIcon } from "../../shared/components/LoadingIcon";
 
-export const LabelPicker = () => {
+interface Props {
+  selectedLabels: string[];
+  onChange: (labelName: string) => void;
+}
+
+export const LabelPicker: FC<Props> = ({ selectedLabels, onChange }) => {
+  const { labelsQuery } = useLabels();
+
+  /**
+   * isLoading => se ejecuta en la primera carga cuando no tenemos ninguna data
+   * isFetching => se ejecuta cada vez que se vuelve a requerir una nueva data
+   */
+  if (labelsQuery.isLoading) return <LoadingIcon />;
+
   return (
     <div>
-        <span 
-            className="badge rounded-pill m-1 label-picker"
-            style={{ border: `1px solid #ffccd3`, color: '#ffccd3' }}
+      {labelsQuery.data?.map((label) => (
+        <span
+          className={`badge rounded-pill m-1 label-picker ${
+            selectedLabels.includes(label.name) ? "label-active" : ""
+          }`}
+          style={{
+            border: `1px solid #${label.color}`,
+            color: `#${label.color}`,
+          }}
+          key={label.id}
+          onClick={() => onChange(label.name)}
         >
-            Primary
+          {label.name}
         </span>
-        
+      ))}
     </div>
-  )
-}
+  );
+};
