5e2273c71684d40939a78f2e0d781347b6962971
Jahiker <rojasjahiker@gmail.com>
2023-11-06 00:07:02 -0400

    [add] listViewInfinite

--- src/api/githubApi.ts
+++ src/api/githubApi.ts
@@ -4,6 +4,6 @@ export const githubApi = axios.create({
   baseURL: "https://api.github.com/repos/facebook/react",
   headers: {
     Authorization:
-      "Bearer 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",
+      "Bearer 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",
   },
 });
